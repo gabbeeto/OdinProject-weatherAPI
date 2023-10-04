8a589da50714932e6016c6e75f3389123d285466
Gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2023-10-04 17:43:22 -0300

    make a button, a search input, a form and link them with the fetch request

--- input/functionality/weather.js
+++ input/functionality/weather.js
@@ -1,14 +1,20 @@
-// let search = prompt('enter city');
+
+
+
+let button = document.querySelector(`form button`);
+button.addEventListener('click', updateItems);
+
+function updateItems(){
+
+let search = document.querySelector(`form input[type='search']`).value;
+
 let weatherUrl = new URL(`http://api.weatherapi.com/v1/current.json?key=61b6b8b8c07140b38b6114538232109`);
 weatherUrl.search = `?key=61b6b8b8c07140b38b6114538232109&q=${search}&aqi=no `
-
 
 let header = new Headers({mode:'cors', method:'GET'})
 
 let weatherRequest = new Request(weatherUrl, header);
-
-
-// fetch(weatherRequest).then(testIfitWorks).then(interactWithJson)
+fetch(weatherRequest).then(testIfitWorks).then(interactWithJson)
 
 function testIfitWorks(response){
   if(response.ok){
@@ -18,5 +24,8 @@ function testIfitWorks(response){
 
 function interactWithJson(jsonResponse){
 console.log(jsonResponse)
+
+}
+
 
 }
