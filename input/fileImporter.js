7a64bcdb4baf42e7451748d611d6b4277ae6de55
Gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2023-10-06 17:16:42 -0300

    add some default styling

--- input/fileImporter.js
+++ input/fileImporter.js
@@ -2,3 +2,6 @@ import './functionality/weather.js'
 import './functionality/search.js'
 import './functionality/displayFollowingDays.js'
 import './functionality/date.js'
+
+
+import './styling/main.css'
